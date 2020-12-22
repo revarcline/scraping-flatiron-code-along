76bf5a2742cfd204eb9e9b8896a3f432976719b4
Alex Cline <rev.a.r.cline@gmail.com>
2020-12-22 14:15:50 -0600

    commit tree 06ef67989e1c15441433621b3ee8716097a7c321

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,18 @@
+# what if we kissed :o in the class instance variable :s
+class Course
+  attr_accessor :title, :schedule, :description
 
+  @all = []
 
+  class << self
+    attr_accessor :all
+  end
+
+  def initialize
+    self.class.all << self
+  end
+
+  def self.reset_all
+    self.all = []
+  end
+end
